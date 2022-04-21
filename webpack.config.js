6464af2cedd136668facc9812bdac274e9082771
Blake Smith <blakeinvictoria@gmail.com>
2022-04-21 09:25:14 -0700

    setup webpack

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+module.exports = {
+  mode: "production",
+  entry: {
+    background: path.resolve(__dirname, ".", "src", "background.ts"),
+    content: path.resolve(__dirname, ".", "src", "content.ts"),
+  },
+  output: {
+    path: path.join(__dirname, "./dist"),
+    filename: "[name].js",
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: "./manifest.json",
+          to: "./manifest.json",
+          context: ".",
+        },
+        {
+          from: "./stylesheet.css",
+          to: "./stylesheet.css",
+          context: ".",
+        },
+      ],
+    }),
+  ],
+};
