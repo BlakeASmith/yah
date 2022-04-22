ec07ccddcbf0db3b73d2d3e35781fda8d63d7a9a
Blake Smith <blakeinvictoria@gmail.com>
2022-04-21 23:14:20 -0700

    refactor: separate functions and add jdoc

--- src/content.ts
+++ src/content.ts
@@ -1,64 +1,122 @@
 import $ from "jquery";
-import tippy from 'tippy.js';
+import tippy from "tippy.js";
 
-type UnwrapFunction = () => void
+type UnwrapFunction = () => void;
 
 const WRAPPER_CLASS_NAME = "iswrapper";
 
+/**
+ * Generate HTML code for a tooltip element.
+ */
 function tooltipHTML() {
-    return `<button>TOOLTIP</button>`;
+  return `<button>TOOLTIP</button>`;
 }
 
-function wrapTextNodes(textElement: Node): [Node, UnwrapFunction] {
-    let wrapper: HTMLElement | undefined = undefined;
-    while (textElement instanceof Text) {
-        wrapper = document.createElement('span');
-        wrapper.classList.toggle(WRAPPER_CLASS_NAME);
-        textElement.parentNode?.insertBefore(wrapper, textElement);
-        textElement.parentNode?.removeChild(textElement);
-        wrapper.appendChild(textElement);
-        textElement = wrapper;
-    }
+/**
+ * Wrap Text nodes in aspan. If a non-text node is provided it will
+ * not be wrapped.
+ *
+ * This allows the tooltip element to be attached to the end of the
+ * highlighted region.
+ *
+ * @param {Node} textElement - The node to be wrapped
+ *
+ * @return {[HTMLElement, UnwrapFunction]} - The wrapper node (a span) and a
+ *     function which will remove the wrapper node from the DOM
+ */
+function wrapTextNodes(textElement: Node): [HTMLElement, UnwrapFunction] {
+  let wrapper: HTMLElement | undefined = undefined;
+  while (textElement instanceof Text) {
+    wrapper = document.createElement("span");
+    wrapper.classList.toggle(WRAPPER_CLASS_NAME);
+    textElement.parentNode?.insertBefore(wrapper, textElement);
+    textElement.parentNode?.removeChild(textElement);
+    wrapper.appendChild(textElement);
+    textElement = wrapper;
+  }
 
-    return [textElement, () => {
-        if (wrapper === undefined) return;
-        console.log("unwrapping")
-        console.log(wrapper)
-        if (wrapper.classList.contains(WRAPPER_CLASS_NAME))
-            wrapper.parentElement?.replaceChild(wrapper.childNodes[0], wrapper);
-    }];
+  return [
+    textElement as HTMLElement,
+    () => {
+      if (wrapper === undefined) return;
+      if (wrapper.classList.contains(WRAPPER_CLASS_NAME))
+        wrapper.parentElement?.replaceChild(wrapper.childNodes[0], wrapper);
+    },
+  ];
 }
 
+/**
+ * @type {boolean} - Flag to prevent multiple instances of the
+ *     tooltip from being visible at the same time
+ */
 let tooltipVisible: boolean = false;
 
-document.addEventListener('mouseup', () => {
-    if (tooltipVisible) return;
+/**
+ * Get the selection range only if the selection is not empty.
+ *
+ * @param {Selection} selection - The current selection
+ * @returns {Range | undefined} - The selection range, or undefined
+ */
+function getSelectionRange(
+  selection: Selection | undefined | null
+): Range | undefined {
+  const range = selection?.getRangeAt(0);
+  return range?.toString()?.length == 0 ? undefined : range;
+}
 
-    const selection = window.getSelection()?.getRangeAt(0);
+/**
+ * Create the highlighting tooltip over an HTML element.
+ *
+ * The tooltip will be destroyed on the next 'mouseup' event, when the
+ * user clicks away from the selection or makes another selection.
+ *
+ * @param {HTMLElement} elem: The element to attach to
+ */
+function attachTooltipToElement(elem: HTMLElement) {
+  if (tooltipVisible) return undefined;
 
-    if (selection === undefined || selection.toString().length == 0) return;
+  tooltipVisible = true;
 
-    const [end, unwrap] = wrapTextNodes(selection.endContainer);
+  const tip = tippy(elem, {
+    content: tooltipHTML(),
+    interactive: true,
+    allowHTML: true,
+    showOnCreate: true,
+    sticky: true,
+    trigger: "manual",
+  });
 
-    const tip = tippy(end as HTMLElement, {
-        content: tooltipHTML(),
-        interactive: true,
-        allowHTML: true,
-        showOnCreate: true,
-        sticky: true,
-        theme: 'tomato',
-        trigger: 'manual',
-    });
+  const destroyTip = () => {
+    tip.destroy();
+    tooltipVisible = false;
+  };
 
-    tooltipVisible = true;
+  document.addEventListener("mouseup", destroyTip, { once: true });
 
-    const destroyTip = () => {
-        tip.destroy();
-        tooltipVisible = false;
-        document.removeEventListener('mouseup', destroyTip);
+  return tip;
+}
 
-        unwrap();
-    }
+/**
+ * Create the highlighting tooltip over the currently selected element.
+ *
+ * Text nodes will be wrapped temporarily with a span so that the
+ * tooltip can be properly attached.
+ */
+function attachTooltipToSelection() {
+  const selection = window.getSelection();
+  const range = getSelectionRange(selection);
 
-    document.addEventListener('mouseup', destroyTip);
-});
+  if (range === undefined) return undefined;
+  const [end, unwrap] = wrapTextNodes(range.endContainer);
+
+  attachTooltipToElement(end);
+
+  document.addEventListener("mouseup", unwrap, { once: true });
+  document.addEventListener(
+    "mousedown",
+    selection!!.removeAllRanges.bind(selection),
+    { once: true }
+  );
+}
+
+document.addEventListener("mouseup", attachTooltipToSelection);
